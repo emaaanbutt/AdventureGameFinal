337880765dfe9a8a200d879ec7b215d13f9fdc66
Ayesha tahir <ayeshatahir582@gmail.com>
2024-10-01 12:28:11 +0500

    My contribution -SP23-BAI-019

--- gameplay.py
+++ gameplay.py
@@ -1,5 +1,101 @@
 import Map as m
 import Menu
+
+
+
+
+
+
+
+
+def displayInventory():
+    if len(inventory) == 0:
+        print("There is nothing in your inventory")
+    else:
+        print("The items in your inventory are: ")
+        print(inventory)
+
+
+def useKey(item):
+    global current_room
+    global inventory
+    global room
+    rooms = m.gameState['rooms']
+
+    if len(inventory) == 0:
+        print("There is nothing in your inventory")
+        return
+
+    if item in inventory:
+        if item in m.keys.keys():
+            c_room = m.keys[item]['c_room']
+            if m.keys[item]['prev_room'] == room:
+                if rooms[c_room]['Locked']:
+                    if rooms[c_room]['Unlocked_By'][0] == item:
+                        rooms[c_room]['Locked'] = False
+                        print(WHITE_BG+color+f"{character}!"+RESET+f" you have used the {item} to unlock the door to the {c_room}.")
+                    else:
+                        print(f"The {item} does not matches with the key to unlock {c_room}.")
+                else:
+                    print(f"The {c_room} is already unlocked. Move ahead...")
+            else:
+                print(f"The {item}  cannot be used to unlock {c_room}")
+        else:
+            print(f"The {item} does not exist.")
+    else:
+        print(f"This {item} is not present in your inventory.")
+
+
+def useItem(item):
+    if item in m.Items.keys():
+        if m.Items[item]['Room'] == room:
+            print(m.Items[item]['Use'])
+        else:
+            print(WHITE_BG+color+f"{character}!"+RESET+f" This {item} seems to have lost its purpose in this place—try using it somewhere else!")
+    else:
+        print(WHITE_BG+color+f"{character}!"+RESET+f" This {item} cannot be used at all. Try something else.")
+
+
+def takeItem(curr_room, item):
+    if item in curr_room["Items"]:
+        inventory.append(item)
+        print(f"The {item} has been added to the inventory.")
+        curr_room['Items'].remove(item)
+    elif item in inventory:
+        print(f"The {item} is already present in the inventory.")
+    else:
+        print(f"The {item} cannot be taken.")
+
+
+def dropItem(curr_room, item):
+    if item in inventory:
+        curr_room['Items'].append(item)
+        inventory.remove(item)  # this line will remove the item from the inventory
+        print(f"You have dropped the item into the {room}")
+    else:
+        print(f"The {item} is not present in the inventory.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def menu():
     print("\n1. Start the Adventure")
     print("2. Customize Your Character")
@@ -32,6 +128,7 @@ def menu():
     else:
         print("Invalid choice.")
         menu()
+
 
 
 def main():
